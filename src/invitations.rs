1e2a741230f9feb908bf3360ae91f90de4887221
yottalogical <email@yottalogical.com>
2022-06-24 10:45:55 -0400

    Implement billing mod

--- src/invitations.rs
+++ src/invitations.rs
@@ -4,7 +4,11 @@
 //!
 //! [api]: https://developers.deepgram.com/api-reference/#invitations
 
-use crate::Deepgram;
+use crate::{send_and_translate_response, Deepgram};
+
+pub mod response;
+
+use response::Message;
 
 /// Manage the invitations to a Deepgram Project.
 ///
@@ -27,5 +31,44 @@ impl<'a, K: AsRef<str>> From<&'a Deepgram<K>> for Invitations<'a, K> {
     /// Construct a new [`Invitations`] from a [`Deepgram`].
     fn from(deepgram: &'a Deepgram<K>) -> Self {
         Self(deepgram)
+    }
+}
+
+impl<K: AsRef<str>> Invitations<'_, K> {
+    /// Remove the authenticated account from the specified project.
+    ///
+    /// See the [Deepgram API Reference][api] for more info.
+    ///
+    /// [api]: https://developers.deepgram.com/api-reference/#invitations
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// # use std::env;
+    /// #
+    /// # use deepgram::{Deepgram, DeepgramError};
+    /// #
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), DeepgramError> {
+    /// # let deepgram_api_key =
+    /// #     env::var("DEEPGRAM_API_KEY").expect("DEEPGRAM_API_KEY environmental variable");
+    /// #
+    /// # let project_id =
+    /// #     env::var("DEEPGRAM_PROJECT_ID").expect("DEEPGRAM_PROJECT_ID environmental variable");
+    /// #
+    /// let dg_client = Deepgram::new(&deepgram_api_key);
+    ///
+    /// dg_client
+    ///     .invitations()
+    ///     .leave_project(&project_id)
+    ///     .await?;
+    /// #
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn leave_project(&self, project_id: &str) -> crate::Result<Message> {
+        let url = format!("https://api.deepgram.com/v1/projects/{}/leave", project_id,);
+
+        send_and_translate_response(self.0.client.delete(url)).await
     }
 }
