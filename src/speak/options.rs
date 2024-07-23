314f92c29d5e7b85bf0e67ca609ea3caef5168de
Damien Murphy <damien.murphy@deepgram.com>
2024-07-23 15:52:07 -0700

    fix build errors

--- src/speak/options.rs
+++ src/speak/options.rs
@@ -165,6 +165,8 @@ pub struct Options {
 ///
 /// Use it to set any of Deepgram's features except the Callback feature.
 /// The Callback feature can be set when making the request by calling [`Transcription::prerecorded_callback`](crate::Speak::speak_to_file).
+///
+/// [builder]: https://rust-unofficial.github.io/patterns/patterns/creational/builder.html
 #[derive(Debug, PartialEq, Clone)]
 pub struct OptionsBuilder(Options);
 
