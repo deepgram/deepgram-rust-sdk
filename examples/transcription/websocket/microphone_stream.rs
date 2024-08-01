5fced41d8b88ad9bee1aafe6fd6cb21ff5670813
Damien Murphy <damien.murphy@deepgram.com>
2024-08-01 14:47:26 -0700

    remove Some(&) as not needed

--- examples/transcription/websocket/microphone_stream.rs
+++ examples/transcription/websocket/microphone_stream.rs
@@ -124,7 +124,7 @@ async fn main() -> Result<(), DeepgramError> {
 
     let (connection, mut response_stream) = dg
         .transcription()
-        .stream_request_with_options(Some(&options))
+        .stream_request_with_options(options)
         .keep_alive()
         .stream(microphone_as_stream())
         .encoding(Encoding::Linear16)
